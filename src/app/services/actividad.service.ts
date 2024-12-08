4dbde74f8662cd277b197f29f106ec63eff056db
Foxriel13 <luismiguelbravo13@gmail.com>
2024-12-08 19:14:17 +0100

    Retoques

--- src/app/services/actividad.service.ts
+++ src/app/services/actividad.service.ts
@@ -11,7 +11,7 @@ export class ActividadService {
   getActividades(): Actividad[] {
     return [
       { id: 1,  fecha: "10/08/2014 00:00", lugar: "Aula02", monitor: "Miguel Goyena", tipo: "BodyPump" },
-      { id: 2,  fecha: "10/08/2014 00:00", lugar: "Aula02", monitor: "Miguel Goyena", tipo: "BodyPump" }
+      { id: 2,  fecha: "12/09/2022 00:00", lugar: "Aula01", monitor: "Miguel Goyena", tipo: "Pilates" }
     ];
   }
 }
